dff10019c180b1c0533a7c80c2c7690acc41cd1d
ValeriaNevarez <mvno.071095@hotmail.com>
2024-08-24 12:08:24 -0700

    Configured and ran Prettier
    Fixed blank spaces and indentations.

--- sketch.js
+++ sketch.js
@@ -21,23 +21,21 @@ function setup() {
 		// Inside the for loop:
 
 		// Set x and y position to be the center
-		x.push(windowWidth / 2)
-		y.push(windowHeight / 2)
+		x.push(windowWidth / 2);
+		y.push(windowHeight / 2);
 
 		// Set the speeds to be random
-		xSpeed.push(Math.random() * (10) - 5);
-		ySpeed.push(Math.random() * (10) - 5);
+		xSpeed.push(Math.random() * 10 - 5);
+		ySpeed.push(Math.random() * 10 - 5);
 
 		// Set the size to be random
-		size.push(Math.random() * (40) + 10);
+		size.push(Math.random() * 40 + 10);
 
-		// Set the colors to be random      
+		// Set the colors to be random
 		r.push(Math.floor(Math.random() * 256));
 		g.push(Math.floor(Math.random() * 256));
 		b.push(Math.floor(Math.random() * 256));
 	}
-
-
 }
 
 function draw() {
@@ -47,37 +45,28 @@ function draw() {
 	for (let i = 0; i < ballCount; i++) {
 		// Inside the for loop:
 		// Increment speed for x position
-		x[i] = x[i] + xSpeed[i]
+		x[i] = x[i] + xSpeed[i];
 
 		// Increment speed for y position
-		y[i] = y[i] + ySpeed[i]
-
+		y[i] = y[i] + ySpeed[i];
 
 		// Reverse x direction if ball hits left or right sides
 		if (x[i] < 0 || windowWidth < x[i]) {
-			xSpeed[i] = xSpeed[i] * -1
+			xSpeed[i] = xSpeed[i] * -1;
 		}
 
 		// Reverse y direction if ball hits top or bottom sides
 		if (y[i] < 0 || windowHeight < y[i]) {
-			ySpeed[i] = ySpeed[i] * -1
+			ySpeed[i] = ySpeed[i] * -1;
 		}
 
-
 		// Set random R, G, B values
-		fill(r[i], g[i], b[i])
-
+		fill(r[i], g[i], b[i]);
 
 		// Style to have no strokes
-		noStroke()
-
+		noStroke();
 
 		// Draw the bouncing balls
-		ellipse(x[i], y[i], (size[i] / 2), (size[i] / 2));
-
-
+		ellipse(x[i], y[i], size[i] / 2, size[i] / 2);
 	}
-
-
-
-} 
+}
